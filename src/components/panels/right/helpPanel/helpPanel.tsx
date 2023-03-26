5d91126454c440b1bfc7c9b018f94f3a9b584ef8
capitalj22 <jjdunn22@gmail.com>
2023-03-26 15:01:14 -0400

    better help

--- src/components/panels/right/helpPanel/helpPanel.tsx
+++ src/components/panels/right/helpPanel/helpPanel.tsx
@@ -1,27 +1,45 @@
 import { Accordion } from "../../../layout/accordion/accordion";
-const example1 = `{myParam}This only appears when 'myParam' is at least 1{/myParam}`;
-const example2 = "%myParam% will be replaced with the value of 'myParam'";
-const example3 = `You make 1 melee attack against an adjacent enemy. Roll 1d6{dmg}+%dmg%{/dmg} and do that much damage.`;
+
+const conditionalParam = `{myParam}This only appears when 'myParam' is at least 1{/myParam}`;
+const valueParam = "%myParam% will be replaced with the value of 'myParam'";
+const localAbility = `You make 1 melee attack against an adjacent enemy. Roll 1d6{dmg}+%dmg%{/dmg} and do that much damage.`;
+
+const gconditionalParam = `{@myParam}This only appears when 'myParam' is at least 1{/myParam}`;
+const gvalueParam = "%@myParam% will be replaced with the value of 'myParam'";
+const glocalAbility = `You make 1 melee attack against an adjacent enemy. Roll 1d6{dmg}+%dmg%{/dmg} and do that much damage.`;
 
 export function HelpPanel() {
   return (
-    <div>
+    <div className="help">
       <Accordion name="Description Syntax" startOpen={false}>
         <div className="padding-md" style={{ textAlign: "left" }}>
+          <h2 className="text-info book">Ability Params:</h2>
           <h3>Condition</h3>
-          <p>{example1}</p>
+          <p>{conditionalParam}</p>
           <h3>Value</h3>
-          <p>{example2}</p>
+          <p>{valueParam}</p>
           <h3>Example</h3>
-          <p>{example3}</p>
+          <p>{localAbility}</p>
+
+          <h2 className="text-info book">Global Params:</h2>
+          <h3>Condition</h3>
+          <p>{gconditionalParam}</p>
+          <h3>Value</h3>
+          <p>{gvalueParam}</p>
+          <h3>Example</h3>
+          <p>{glocalAbility}</p>
         </div>
       </Accordion>
       <Accordion name="Hotkeys" startOpen={true}>
         <div className="padding-md" style={{ textAlign: "left" }}>
           <p>
             <ul>
-              <li><span className="bold">Shift + E</span> - Toggle Edit Mode</li>
-              <li><span className="bold">Shift + F</span> - Toggle Fast Build Mode</li>
+              <li>
+                <span className="bold">Shift + E</span> - Toggle Edit Mode
+              </li>
+              <li>
+                <span className="bold">Shift + F</span> - Toggle Fast Build Mode
+              </li>
             </ul>
           </p>
         </div>
