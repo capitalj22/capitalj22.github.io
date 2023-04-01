b4865bb41ee644d716c7a8adbb027cd01be5a33e
capitalj22 <jjdunn22@gmail.com>
2023-04-01 15:24:23 -0400

    cleanup

--- src/components/panels/right/rightPanel.tsx
+++ src/components/panels/right/rightPanel.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-
 import { AppPanel } from "../../layout/appPanel/appPanel";
 import { SidebarRight } from "../../layout/right/sidebarRight";
 
 export function RightPanel({ graphEvents$ }) {
-  const [menuTitle, setMenuTitle] = useState("info");
-
   return (
-    <SidebarRight title={menuTitle}>
+    <SidebarRight>
       <AppPanel graphEvents$={graphEvents$} side="right" />
     </SidebarRight>
   );
