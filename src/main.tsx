4eaa372e37fa070ff599b6f512f6430351ed3566
capitalj22 <jjdunn22@gmail.com>
2024-05-19 15:38:37 -0400

    try hashrouter 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { CardBuilder } from "./card-builder/CardBuilder";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "cards",
+        element: <CardBuilder />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <HashRouter>
-    <Routes>
-      <Route index element={<App />} />
-      <Route path="cards" element={<CardBuilder/>}/>
-    </Routes>
-  </HashRouter>
+  <RouterProvider router={router} />
 );
