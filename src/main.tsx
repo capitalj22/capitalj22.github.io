d61e34f1fb32de01231407b4bfa9a002dcbce109
capitalj22 <jjdunn22@gmail.com>
2024-05-19 15:34:34 -0400

    try to use hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { CardBuilder } from "./card-builder/CardBuilder";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route index element={<App />} />
       <Route path="cards" element={<CardBuilder/>}/>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
